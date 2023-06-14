5dca8343771198f28a36e21abf82f80d34f84950
midulislamrasel1 <midulraddy121@gmail.com>
2023-06-14 17:31:33 +0600

    Done server

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+
+
+async function boostrap() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log(`✅🟢 Database is connected successfully`)
+
+        app.listen(config.port, () => {
+            console.log(` Application listening on port ${config.port}`)
+        })
+
+    } catch (err) {
+        console.log('❌🔴 Faild to connect Database', err)
+    }
+
+}
+
+boostrap();
